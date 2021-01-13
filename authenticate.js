bf0d3d65ebe2129a9407d3ba87c83ca8f6899989
Shikhar Goel <shikhargoel4195@gmail.com>
2021-01-14 00:46:17 +0530

    Updated login form

--- authenticate.js
+++ authenticate.js
@@ -38,6 +38,38 @@ showButton();
 hideButton();
 		
 	}
+
+	function otp()
+	{
+		var otp1=document.getElementById("otp1");
+		var phone=document.getElementById("phone");
+		//e.preventDefault();
+		fetch('https://rocky-bayou-35696.herokuapp.com/profile/login',{
+			method: 'POST',
+			headers:{
+				'Accept':'application/json',
+				'Content-Type':'application/json'
+			},
+			body: JSON.stringify({
+		username: phone.value,
+		password: otp1.value
+	  }),
+	   credentials: "same-origin"
+	})
+	 .then((response) => response.json())
+	 .then((responseJson) => {
+		console.log(responseJson.access_token);
+		const token=btoa(responseJson.access_token)
+		 console.log(token);
+		 window.localStorage.setItem("access_token", responseJson.access_token);
+	 })
+	 .catch((error) => {
+		 console.log("reset client error-------",error);
+	});
+	$('#myModal').modal('hide');
+	
+	}
+	
 	
 	
 	function video()
@@ -88,33 +120,3 @@ hideButton();
 		
 	}
 
-	function otp()
-{
-    var otp1=document.getElementById("otp1");
-    var phone=document.getElementById("phone");
-	//e.preventDefault();
-	fetch('https://rocky-bayou-35696.herokuapp.com/profile/login',{
-		method: 'POST',
-		headers:{
-			'Accept':'application/json',
-			'Content-Type':'application/json'
-		},
-		body: JSON.stringify({
-    username: phone.value,
-    password: otp1.value
-  }),
-   credentials: "same-origin"
-})
- .then((response) => response.json())
- .then((responseJson) => {
-	console.log(responseJson.access_token);
-	const token=btoa(responseJson.access_token)
-     console.log(token);
-	 window.localStorage.setItem("access_token", responseJson.access_token);
- })
- .catch((error) => {
-     console.log("reset client error-------",error);
-});
-$('#myModal').modal('hide');
-
-}
