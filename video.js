9850974ded5dfe782e8dba3c8ec3f809707cc75d
voyagerbharat <42874594+voyagerbharat@users.noreply.github.com>
2021-01-16 13:21:37 +0530

    video-page

--- video.js
+++ video.js
@@ -16,12 +16,12 @@ window.onload=function(){
             {
                 console.log(i , data['videos'][i] );
                 console.log(data['videos'][i]['title']);
-                var disp='<div class="mb-5"><div class="col-sm-3"><iframe id="ytplayer" class="responsive-iframe" type="text/html" width="350px" height="250px"src="https://www.youtube.com/embed/%videoid%?autoplay=0&origin=http://example.com"frameborder="0"></iframe></div><div class="col-sm-2"></div><div class="col-sm-7"><h4>%title%</h4><p>%description%</p></div></div>';
+                var disp='<div class="mb-5 border-bottom mt-3"><div class="col-sm-3"><iframe id="ytplayer" class="responsive-iframe " type="text/html" width="350px" height="250px"src="https://www.youtube.com/embed/%videoid%?autoplay=0&origin=http://example.com"frameborder="0"></iframe></div><div class="col-sm-1"></div><div class="col-sm-8"><h4 class="fontt">%title%</h4><p class="content">%description%</p></div></div>';
                 //var newhtml = html.replace('%id%',data['news'][i]['id']);
                 var newhtml = disp.replace('%videoid%',data['videos'][i]['videoId']);
                 newhtml = newhtml.replace('%title%',data['videos'][i]['title']);
                 newhtml = newhtml.replace('%description%',data['videos'][i]['description']);
-                document.querySelector('.news-add').insertAdjacentHTML('beforeend' , newhtml);
+                document.querySelector('.vid-add').insertAdjacentHTML('beforeend' , newhtml);
                 setTimeout(3000);
             }
             i++;
