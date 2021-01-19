375ebd53feaed37d4d377e145cac2b98d944f92d
voyagerbharat <42874594+voyagerbharat@users.noreply.github.com>
2021-01-19 17:59:11 +0530

    news image in mobile view

--- js/newsView.js
+++ js/newsView.js
@@ -22,7 +22,7 @@ else if(localStorage.getItem("index"))
            
             console.log(i , data['news'][i] );
             console.log(data['news'][i]['title']);
-            var disp='<div class="row justify-content-center"><div class="col-sm-7 mt-3 mx-2"><h1 class="mt-0 fontt break">%title%</h1></div></div><div class="row justify-content-center"><div class="col-sm-8 mx-2"><img class="imgg responsive" style="margin-bottom:10px;" src=%imgsrc% height="500px" width="350px"><h5 class="text-muted">by %author% %date%</h5></div></div><div class="row justify-content-center"><div class="col-sm-12 mt-3 mx-2"><p class="content" style="color:#222224;">%content%</p></div></div>';
+            var disp='<div class="row justify-content-center"><div class="col-sm-7 mt-3 mx-2"><h1 class="mt-0 fontt break">%title%</h1></div></div><div class="row justify-content-center"><div class="col-sm-8 mx-2"><img class="imgg responsive newsimg" style="margin-bottom:10px;" src=%imgsrc% height="500px" width="350px"><h5 class="text-muted">by %author% %date%</h5></div></div><div class="row justify-content-center"><div class="col-sm-12 mt-3 mx-2"><p class="content" style="color:#222224;">%content%</p></div></div>';
             //var newhtml = html.replace('%id%',data['news'][i]['id']);
             var newhtml = disp.replace('%title%',data['news'][i]['title']);
             newhtml = newhtml.replace('%content%',data['news'][i]['content']);
