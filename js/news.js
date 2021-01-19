7804caf038b7124f367c4fb4420fe1d7f6947bd7
voyagerbharat <42874594+voyagerbharat@users.noreply.github.com>
2021-01-19 14:24:16 +0530

    news-page

--- js/news.js
+++ js/news.js
@@ -33,7 +33,7 @@ function newsDisp(){
                         trimmedString+='. . .';
                     }
                    
-                        disp='<div class="my-2 py-3 border border-primary mt-3 row justify-content-between"><div class="col-sm-5 mx-2"><a onclick="newsview(%i%)" href="newsview.html" style="color:black"><img class="imgg responsive" style="margin-bottom:10px;" src=%imgsrc% height="300px" width="350px"></div><div class="col-sm-6 mx-2 "><h2 class="mt-0 fontt break">%title%</h2><h5 class="text-muted">by %author% %date%</h5><p class="content" style="color:#222224;">%content%</p></a></div></div>';
+                        disp='<div class="my-2 py-3 border mt-3 row justify-content-between"><div class="col-sm-5 mx-2"><img class="imgg responsive" style="margin-bottom:10px;" src=%imgsrc% height="300px" width="350px"></div><div class="col-sm-6 mx-2 "><a class="anch" onclick="newsview(%i%)" href="newsview.html"><h2 class="mt-0 fontt break">%title%</h2><h5 class="text-muted">by %author% %date%</h5><p class="content" style="color:#222224;">%content%</p></a></div></div>';
                     //var newhtml = html.replace('%id%',data['news'][i]['id']);
                     var newhtml = disp.replace('%title%',data['news'][i]['title']);
                     newhtml = newhtml.replace('%content%',trimmedString);
--- js/newsView.js
+++ js/newsView.js
@@ -22,7 +22,7 @@ else if(localStorage.getItem("index"))
            
             console.log(i , data['news'][i] );
             console.log(data['news'][i]['title']);
-            var disp='<div class="my-2 py-3 border mt-3 row justify-content-between"><div class="col-sm-5 mx-2"><a onclick="newsview(%i%)" href="#" style="color:black"><img class="imgg responsive" style="margin-bottom:10px;" src=%imgsrc% height="300px" width="350px"></div><div class="col-sm-6 mx-2 "><h2 class="mt-0 fontt break">%title%</h2><h5 class="text-muted">by %author% %date%</h5><p class="content" style="color:#222224;">%content%</p></a></div></div>';
+            var disp='<div class="row justify-content-center"><div class="col-sm-7 mt-3 mx-2"><h1 class="mt-0 fontt break">%title%</h1></div></div><div class="row justify-content-center"><div class="col-sm-8 mx-2"><img class="imgg responsive" style="margin-bottom:10px;" src=%imgsrc% height="500px" width="350px"><h5 class="text-muted">by %author% %date%</h5></div></div><div class="row justify-content-center"><div class="col-sm-12 mt-3 mx-2"><p class="content" style="color:#222224;">%content%</p></div></div>';
             //var newhtml = html.replace('%id%',data['news'][i]['id']);
             var newhtml = disp.replace('%title%',data['news'][i]['title']);
             newhtml = newhtml.replace('%content%',data['news'][i]['content']);
