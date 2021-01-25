93b31cc34fc6c3cae5c16d279870aff37e56b47b
voyagerbharat <42874594+voyagerbharat@users.noreply.github.com>
2021-01-25 22:14:51 +0530

    newsview

--- js/newsView.js
+++ js/newsView.js
@@ -23,7 +23,7 @@ else if(localStorage.getItem("index"))
     $(document).ready(function(){
         var bearer = "Bearer " + localStorage.getItem("access_token");
         var b=baseUrl();
-           fetch(b+'news/getNews?format=json',{
+           fetch(b+'news/getOrganizerNews?format=json',{
                method: 'GET',
                headers:{
                    Authorization:bearer
