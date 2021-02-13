9a50af7800818cd9600d4db48b1af41e0f060ea1
voyagerbharat <42874594+voyagerbharat@users.noreply.github.com>
2021-02-13 23:03:18 +0530

    index phone page

--- js/news.js
+++ js/news.js
@@ -127,7 +127,7 @@ function newsDisp(){
                             {
                                 trimmedString+='. . .';
                             }
-                            disp='<div class="my-2 py-3 border mt-3 row shadow rc justify-content-between"><div class="col-sm-5 mx-2"><img class="imgg responsive" style="margin-bottom:10px;" src=%imgsrc% height="300px" width="350px"></div><div class="col-sm-6 mx-2 "><a class="anch" onclick="newsview(%i%)" href="newsview.html"><h2 class="mt-0 fontt break">%title%</h2><h5 class="text-muted">by %author% %date%</h5><p class="content" style="color:#222224;">%content%</p></a></div></div>';
+                            disp='<div class="my-2 py-5 border mt-3 row shadow rc justify-content-between"><div class="col-sm-5 mx-2"><img class="imgg responsive" style="margin-bottom:10px;" src=%imgsrc% height="300px" width="350px"></div><div class="col-sm-6 mx-2 "><a class="anch" onclick="newsview(%i%)" href="newsview.html"><h2 class="mt-0 fontt break">%title%</h2><h5 class="text-muted">by %author% %date%</h5><p class="content" style="color:#222224;">%content%</p></a></div></div>';
                             //var newhtml = html.replace('%id%',data['news'][i]['id']);
                             var newhtml = disp.replace('%title%',data['news'][i]['title']);
                             newhtml = newhtml.replace('%content%',trimmedString);
